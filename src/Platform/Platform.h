5273f306b7dbd3329609045f5a089f84d8d31def
magnus <gjerde.magnus@gmail.com>
2021-04-11 10:32:37 +0200

    Removed shader debug

--- src/Platform/Platform.h
+++ src/Platform/Platform.h
@@ -49,7 +49,7 @@ private:
     // - Extensions
     const std::vector<const char*> deviceExtensions = {
             VK_KHR_SWAPCHAIN_EXTENSION_NAME,
-            VK_KHR_SHADER_NON_SEMANTIC_INFO_EXTENSION_NAME
+            //VK_KHR_SHADER_NON_SEMANTIC_INFO_EXTENSION_NAME
     };
 
     // Create functions
