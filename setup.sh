18337d709ddd3402b08e4c9d0e5277d11a50181c
magnus <gjerde.magnus@gmail.com>
2022-01-13 13:13:32 +0100

    Started on setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt install libglm-dev libglfw3-dev
+
+# Update graphics driver to enable vulkan support
+# cmake gcc etc..
+
+# Create Assets folder
+mkdir "Assets"
+
+# Clone models from an online database
